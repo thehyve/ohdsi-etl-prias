709c22bd9648d9eb1d55475b1acd239b40861bf9
Maxim Moinat <maxim@thehyve.nl>
2019-10-21 17:50:53 +0200

    add copyright and licensing to cdm model

--- src/main/python/model/cdm/__init__.py
+++ src/main/python/model/cdm/__init__.py
@@ -1,8 +1,7 @@
-"""
-Version 6 with oncology extension, and death table from v5.3.1.
-Generated with python using sqlacodegen  package on 2019-10-21,
- model from https://github.com/OHDSI/CommonDataModel/tree/Dev/PostgreSQL, commit 30d851a.
-"""
+# OMOP CDM Version 6 with oncology extension, and death table from v5.3.1.
+# Generated with python using sqlacodegen package on 2019-10-21,
+#  model from https://github.com/OHDSI/CommonDataModel/tree/Dev/PostgreSQL, commit 30d851a.
+
 from .clinical_data import *
 from .derived_elements import *
 from .vocabularies import *
--- src/main/python/model/cdm/clinical_data.py
+++ src/main/python/model/cdm/clinical_data.py
@@ -1,3 +1,20 @@
+# Copyright 2019 The Hyve
+#
+# Licensed under the GNU General Public License, version 3,
+# or (at your option) any later version (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.gnu.org/licenses/
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# OMOP CDM v6, with oncology extension, defined by https://github.com/OHDSI/CommonDataModel/tree/Dev/PostgreSQL #30d851a
+
+# !/usr/bin/env python3
 # coding: utf-8
 from sqlalchemy import BigInteger, Column, Date, DateTime, ForeignKey, Integer, Numeric, String, Text
 from sqlalchemy.orm import relationship
