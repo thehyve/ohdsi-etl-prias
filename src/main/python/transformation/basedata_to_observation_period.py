c2efe8aeb07222e200ef51cbd66fa96c252de5bd
EmV25 <emma@thehyve.nl>
2020-02-10 13:49:57 +0100

    change to basedata_to_observation, to include all patients

--- src/main/python/transformation/basedata_to_observation_period.py
+++ src/main/python/transformation/basedata_to_observation_period.py
@@ -16,25 +16,26 @@
 from src.main.python.model.cdm import ObservationPeriod
 from datetime import date
 
-def enddata_to_observation_period(wrapper) -> list:
-    source_data = wrapper.get_enddata()
+def basedata_to_observation_period(wrapper) -> list:
+    base_data = wrapper.get_basedata()
     extraction_date = date(2099, 7, 1)
 
     records_to_insert = []
-    for row in source_data:
+    for row in base_data:
 
-        basedata_record = wrapper.lookup_basedata_by_pid(row['p_id'])
-        basedata_start_date = date(int(basedata_record['year_diagnosis']), 7, 1)
+        basedata_start_date = date(int(row['year_diagnosis']), 7, 1)
 
-        if row['year_discontinued'] != '':
-            date_discontinued = date(int(row['year_discontinued']), 7, 1)
+        # If there is a year_discontinued value in the enddate table, use for observation_period_end_date
+        enddata_record = wrapper.lookup_enddata_by_pid(row['p_id'])
+        if enddata_record is not None and enddata_record['year_discontinued'] != '':
+            observation_period_end_date = date(int(enddata_record['year_discontinued']), 7, 1)
         else:
-            date_discontinued = extraction_date
+            observation_period_end_date = extraction_date
 
         record = ObservationPeriod(
             person_id=int(row['p_id']),
             observation_period_start_date=basedata_start_date,
-            observation_period_end_date=date_discontinued,
+            observation_period_end_date=observation_period_end_date,
             period_type_concept_id=44814723  # Period while enrolled in study
         )
         records_to_insert.append(record)
@@ -47,5 +48,5 @@ if __name__ == '__main__':
 
     db = Database('postgresql://postgres@localhost:5432/postgres')  # A mock database object
     w = Wrapper(db, '../../../../resources/source_data', '../../../../resources/mapping_tables')
-    for x in enddata_to_observation_period(w):
+    for x in basedata_to_observation_period(w):
         print(x.__dict__)
