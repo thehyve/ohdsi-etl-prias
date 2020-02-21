2e099d00dc2b3c09c9f6831e201fe89312999d8c
Maxim Moinat <maxim@thehyve.nl>
2020-02-21 14:32:54 +0100

    refactor mapping to episode

--- src/main/python/transformation/basedata_to_episode.py
+++ src/main/python/transformation/basedata_to_episode.py
@@ -16,6 +16,7 @@
 from src.main.python.util.create_record_source_value import create_episode_record_source_value
 from datetime import datetime
 from src.main.python.model.cdm import Episode
+import copy
 
 
 def basedata_to_episode(wrapper) -> list:
@@ -25,63 +26,62 @@ def basedata_to_episode(wrapper) -> list:
 
     records_to_insert = []
 
-    # Episode groups
-    lesion1_variables = ['mri_pirads_1.0', 'mri_largest_dia_1.0', 'mri_location_1.0', 'mri_location_free_1.0']
-    lesion2_variables = ['mri_pirads_2.0', 'mri_largest_dia_2.0', 'mri_location_2.0', 'mri_location_free_2.0']
-    lesion3_variables = ['mri_pirads_3.0', 'mri_largest_dia_3.0', 'mri_location_3.0', 'mri_location_free_3.0']
-    core_biopsy1_variables = ['num_cores', 'num_cores_pc', 'gleason1']
-    core_biopsy2_variables = ['num_cores2', 'num_cores_pc2', 'gleason1_2']
+    # Episode groups.
+    # NOTE: The last character of the group name (key) is taken as the episode number
+    lesion_episode_groups = {
+        'lesion1': ['mri_pirads_1.0', 'mri_largest_dia_1.0', 'mri_location_1.0', 'mri_location_free_1.0'],
+        'lesion2': ['mri_pirads_2.0', 'mri_largest_dia_2.0', 'mri_location_2.0', 'mri_location_free_2.0'],
+        'lesion3': ['mri_pirads_3.0', 'mri_largest_dia_3.0', 'mri_location_3.0', 'mri_location_free_3.0']
+    }
 
-    episode_groups = {'lesion1': lesion1_variables, 'lesion2': lesion2_variables, 'lesion3': lesion3_variables,
-                      'core_biopsy1': core_biopsy1_variables, 'core_biopsy2': core_biopsy2_variables}
+    biopsy_episode_groups = {
+        'core_biopsy1': ['num_cores', 'num_cores_pc', 'gleason1'],  # Note: the variable names are not used here
+        'core_biopsy2': ['num_cores2', 'num_cores_pc2', 'gleason1_2']
+    }
 
     for row in basedata:
 
+        base_episode_record = Episode(
+            person_id=int(row['p_id']),
+            episode_start_datetime=datetime(int(row['year_diagnosis']), 7, 1),
+            episode_end_datetime=datetime(int(row['year_diagnosis']), 7, 1),
+            episode_object_concept_id=0,
+            episode_source_concept_id=0,
+            episode_type_concept_id=0
+        )
+
         # Go through each episode group
-        for episode_group in episode_groups:
+        for episode_group, episode_variables in lesion_episode_groups.items():
+            lesion_values = [row[variable] for variable in episode_variables]
 
-            if episode_group.startswith('lesion'):
-                episode_concept_id = 4115735,  # Lesion of prostate (condition occurrence concept)
+            # Skip if all lesion values are empty or '0'
+            if all(item in [None, '0', ''] for item in lesion_values):
+                continue
 
-                # If all values of the lesion variables are absent or '0', there is no (episode) event
-                lesion_values = [row[variable] for variable in episode_groups[episode_group]]
+            # Skip if no mri was taken
+            if row['mri_taken.0'] != '1':
+                continue
 
-                check_values_absent = all(item in [None, '0', ''] for item in lesion_values)
-                if check_values_absent:
-                    continue
+            episode_lesion = copy.deepcopy(base_episode_record)
+            episode_lesion.episode_concept_id = 4115735  # Lesion of prostate (condition occurrence)
+            episode_lesion.episode_number = int(episode_group[-1:])
+            episode_lesion.record_source_value = create_episode_record_source_value(row['p_id'], source_table_name, episode_group)
+            records_to_insert.append(episode_lesion)
 
-                # Don't map episode when mri_taken.0 is 1
-                if row['mri_taken.0'] != '1':
-                    continue
-
-            # Do not map episode when there num_cores is empty
+        for episode_group in biopsy_episode_groups:
+            # Skip if the number of cores is empty for the given biopsy
             if episode_group == 'core_biopsy1' and row['num_cores'] == '':
                 continue
 
             if episode_group == 'core_biopsy2' and row['num_cores2'] == '':
                 continue
 
-            if episode_group.startswith('core_biopsy'):
-                episode_concept_id = 4221802  # Core needle biopsy of prostate (procedure occurrence concept)
+            episode_biopsy = copy.deepcopy(base_episode_record)
+            episode_biopsy.episode_concept_id = 4221802  # Core needle biopsy of prostate (procedure occurrence)
+            episode_biopsy.episode_number = int(episode_group[-1:])
+            episode_biopsy.record_source_value = create_episode_record_source_value(row['p_id'], source_table_name, episode_group)
 
-            # Create episode record source value
-            episode_record_source_value = create_episode_record_source_value(row['p_id'],
-                                                                             source_table_name,
-                                                                             episode_group)
-
-            record = Episode(
-                person_id=int(row['p_id']),
-                episode_start_datetime=datetime(int(row['year_diagnosis']), 7, 1),
-                episode_end_datetime=datetime(int(row['year_diagnosis']), 7, 1),
-                episode_concept_id=episode_concept_id,
-                episode_number=int(episode_group[-1:]),
-                episode_object_concept_id=0,
-                episode_source_concept_id=0,
-                episode_type_concept_id=0,
-                record_source_value=episode_record_source_value
-            )
-
-            records_to_insert.append(record)
+            records_to_insert.append(episode_biopsy)
 
     return records_to_insert
 
