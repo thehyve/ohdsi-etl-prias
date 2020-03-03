8a653c4a626df7963d6ca3cc22b8f9e9154a8495
Sofia Bazakou <58699690+SofiaMp@users.noreply.github.com>
2020-03-03 10:27:54 +0100

    Delete test_episode.R
    It should be in a different branch with the tests

--- src/test/R/test_cases/test_episode.R
+++
@@ -1,7 +0,0 @@
-#Basedata to Event --------------------------------------------
-declareTest(351, 'Event - lesion_episode_groups')
-add_basedata(p_id='10351', 'mri_pirads_1.0', 'mri_largest_dia_1.0', 
-             'mri_location_1.0', 'mri_location_free_1.0')
-expect_observation(visit_occurrence_id=lookup_observation('visit_occurrence_id',
-                                                          person_id='10340',
-                                                          observation_concept_id='4096978'))
