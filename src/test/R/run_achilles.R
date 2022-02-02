15e8e9de7b70fb8e4a6d9ac2785e7cdabeb67462
Maxim Moinat <maxim@thehyve.nl>
2022-02-02 16:10:27 +0100

    Update run_achilles.R

--- src/test/R/run_achilles.R
+++ src/test/R/run_achilles.R
@@ -7,28 +7,27 @@ library(Achilles)
 
 connectionDetails <- createConnectionDetails(dbms="postgresql",
                                              server="localhost/ohdsi",
-                                             user="admin",
+                                             user="postgres",
                                              password="secret",
                                              port=5432)
 c <- connect(connectionDetails)
-executeSql(connection = c, "CREATE SCHEMA IF NOT EXISTS temp;")
 
 # Run select analyses, excluding some that are not essential and can cause issues.
 analysisDetails <-  Achilles::getAnalysisDetails()
-analysisIds <- analysisDetails[   !(analysisDetails$ANALYSIS_ID >= 900 & analysisDetails$ANALYSIS_ID < 1000) # drug_era
-  & !(analysisDetails$ANALYSIS_ID >= 1400 & analysisDetails$ANALYSIS_ID < 1800), # cost, payer-plan
-  "ANALYSIS_ID"] # cost
+# analysisIds <- analysisDetails[   !(analysisDetails$ANALYSIS_ID >= 900 & analysisDetails$ANALYSIS_ID < 1000) # drug_era
+#   & !(analysisDetails$ANALYSIS_ID >= 1400 & analysisDetails$ANALYSIS_ID < 1800), # cost, payer-plan
+#   "ANALYSIS_ID"] # cost
 
 achillesResults <- achilles(connectionDetails,
                             cdmDatabaseSchema = "public",
-                            resultsDatabaseSchema = "ohdsi",
+                            resultsDatabaseSchema = "prias_results",
                             vocabDatabaseSchema = "vocab",
-                            scratchDatabaseSchema = "temp",
+                            scratchDatabaseSchema = "public",
                             sourceName = "PRIAS",
                             cdmVersion = "5",
-                            smallCellCount = 1,
+                            smallCellCount = 0,
                             analysisIds = analysisIds,
                             runHeel = TRUE,
                             validateSchema = FALSE)
 
-exportToJson(connectionDetails, "public", "ohdsi", vocabDatabaseSchema = "vocab")
+# exportToJson(connectionDetails, "public", "ohdsi", vocabDatabaseSchema = "vocab")
