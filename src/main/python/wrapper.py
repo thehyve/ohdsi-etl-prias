e251b05098985cf9c25b6d2cb9753909a1823b94
Spayralbe <stefan@thehyve.nl>
2023-02-05 22:26:30 +0100

    Remove skipvocab option

--- src/main/python/wrapper.py
+++ src/main/python/wrapper.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 
 class Wrapper(EtlWrapper):
 
-    def __init__(self, database, source_folder, mapping_tables_folder, skipvocab):
+    def __init__(self, database, source_folder, mapping_tables_folder):
         super().__init__(database=database, source_schema='')
         self.source_folder = Path(source_folder)
         self.variable_mapper = VariableConceptMapper(Path(mapping_tables_folder))
@@ -43,7 +43,6 @@ class Wrapper(EtlWrapper):
         self.source_table_basedata = None
         self.source_table_fulong = None
         self.source_table_enddata = None
-        self.skipvocab = skipvocab
         self.fulong_batch_number = 0
         self.FULONG_BATCH_SIZE = 5000
 
@@ -61,28 +60,11 @@ class Wrapper(EtlWrapper):
         # self.execute_sql_file('./postgres/results_ddl_2.7.4.sql')
         self.execute_sql_query('SET search_path TO public;')
 
-        # Vocab schema
-        if not self.skipvocab:
-            print('Dropping vocab schema')
-            # self.execute_sql_query('DROP SCHEMA IF EXISTS vocab CASCADE; CREATE SCHEMA vocab;')
-            logger.info('Vocabulary schema emptied')
-
         # Prepare source
         self.drop_cdm()
         logger.info('Clinical CDM tables dropped')
         self.create_cdm()
         logger.info('CDM tables created')
-
-        # Load vocabulary files
-        if not self.skipvocab:
-            print('Loading vocab files')
-            # self.load_from_csv('vocab_files/VOCABULARY.csv', Vocabulary)
-            # self.load_from_csv('vocab_files/DOMAIN.csv', Domain)
-            # self.load_from_csv('vocab_files/CONCEPT_CLASS.csv', ConceptClass)
-            # self.load_from_csv('vocab_files/CONCEPT_CPT4.csv', Concept)
-            # self.load_from_csv('vocab_files/CONCEPT.csv', Concept)
-            # self.load_from_csv('vocab_files/CONCEPT_ANCESTOR.csv', ConceptAncestor)
-            logger.info('Vocabulary schema loaded')
 
         self.create_vocab_views()  # Views in public schema
         logger.info('Vocabulary views created')
